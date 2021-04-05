e09ccdb2d3ce00be9a314886776a6032d24899a5
Kaloyan Dobrev <k.dobrev@razorlabs.com>
2021-04-05 15:05:49 +0300

    macOS compatability

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -21,14 +21,14 @@ fi
 EXEC_DIR=$(pwd)
 
 TMP_DIR="$HOME/plugintmp"
-rm -Rf "$TMP_DIR"
+rm -rf "$TMP_DIR"
 mkdir -p "$TMP_DIR"
 
-rm -Rf "$TMP_DIR/wp2static"
+rm -rf "$TMP_DIR/wp2static"
 mkdir "$TMP_DIR/wp2static"
 
 # clear dev dependencies
-rm -Rf "$EXEC_DIR/vendor/*"
+rm -rf "$EXEC_DIR/vendor/*"
 # load prod deps and optimize loader
 composer install --quiet --no-dev --optimize-autoloader
 
@@ -44,17 +44,17 @@ cd "$TMP_DIR" || exit
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 
-zip --quiet -r -9 "./$1.zip" ./wp2static
+zip --quiet -r -9 "./wp2static.zip" ./wp2static
 
 cd - || exit
 
 mkdir -p "$HOME/Downloads/"
 
-cp "$TMP_DIR/$1.zip" "$HOME/Downloads/"
+cp "$TMP_DIR/wp2static.zip" "$HOME/Downloads/"
 
 # reset dev dependencies
 cd "$EXEC_DIR" || exit
 # clear dev dependencies
-rm -Rf "$EXEC_DIR/vendor/*"
+rm -rf "$EXEC_DIR/vendor/*"
 # load prod deps
 composer install --quiet
